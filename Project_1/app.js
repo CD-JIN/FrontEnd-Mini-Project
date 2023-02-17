abc4137877a52bf85449235ecbbaa947c2e5cc5e
CD-JIN <92803648+CD-JIN@users.noreply.github.com>
2023-02-18 01:27:59 +0900

    Canvas API - My First Drawing

--- Project_1/app.js
+++ Project_1/app.js
@@ -1,1 +1,8 @@
-alert("hello");
+const canvas = document.querySelector("canvas");
+
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.fillRect(50, 50, 100, 200);
