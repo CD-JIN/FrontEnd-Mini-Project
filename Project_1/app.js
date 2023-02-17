4be816fe7fe2a01a1d97d64a3b805e38c952e9a9
JIN <jin@DESKTOP-IBDTVAC.localdomain>
2023-02-18 02:07:57 +0900

    Canvas API - Drawing simple Human

--- Project_1/app.js
+++ Project_1/app.js
@@ -5,13 +5,15 @@ const ctx = canvas.getContext("2d");
 canvas.width = 700;
 canvas.height = 700;
 
-ctx.fillRect(200, 200, 50, 200);
-ctx.fillRect(400, 200, 50, 200);
+ctx.fillRect(200, 200, 20, 100);
+ctx.fillRect(360, 200, 20, 100);
+ctx.fillRect(260, 200, 60, 200);
 
-ctx.fillRect(300, 300, 50, 100);
-ctx.fillRect(200, 200, 200, 20);
-ctx.moveTo(200, 200);
-ctx.lineTo(325, 150);
-ctx.lineTo(450, 200);
+ctx.arc(290, 150, 40, 0, 2 * Math.PI);
+ctx.fill();
 
+ctx.beginPath();
+ctx.fillStyle = "white";
+ctx.arc(277, 140, 8, Math.PI, 2 * Math.PI);
+ctx.arc(303, 140, 8, Math.PI, 2 * Math.PI);
 ctx.fill();
