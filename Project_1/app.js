df9c4aed9ab6bfcd1014ce2059f4211fac9bffdc
CD-JIN <92803648+CD-JIN@users.noreply.github.com>
2023-02-19 23:40:14 +0900

    Add Eraser, Reset Button

--- Project_1/app.js
+++ Project_1/app.js
@@ -1,4 +1,6 @@
 const modeBtn = document.getElementById("mode-btn");
+const destroyBtn = document.getElementById("destroy-btn");
+const eraserBtn = document.getElementById("eraser-btn");
 const canvas = document.querySelector("canvas");
 const lineWidth = document.getElementById("line-width");
 const color = document.getElementById("color");
@@ -8,8 +10,11 @@ const colorOptions = Array.from(
 
 const ctx = canvas.getContext("2d");
 
-canvas.width = 700;
-canvas.height = 700;
+const CANVAS_WIDTH = 700;
+const CANVAS_HEIGHT = 700;
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 
 ctx.lineWidth = lineWidth.value;
 
@@ -63,8 +68,19 @@ function onModeClick() {
 
 function onCanvasClick() {
   if (isFilling) {
-    ctx.fillRect(0, 0, 700, 700);
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   }
+}
+
+function onDestroyClick() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
+function onEraserClick() {
+  ctx.strokeStyle = "white";
+  isFilling = false;
+  modeBtn.innerText = "Fill";
 }
 
 canvas.addEventListener("mousemove", onMove);
@@ -77,3 +93,5 @@ color.addEventListener("change", onColorChange);
 
 colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
 modeBtn.addEventListener("click", onModeClick);
+destroyBtn.addEventListener("click", onDestroyClick);
+eraserBtn.addEventListener("click", onEraserClick);
